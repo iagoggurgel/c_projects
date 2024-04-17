d745c9a326c2f646994194bcb0b203bd9616705f
Iago Gouveia Gurgel <iagoggurgel25@gmail.com>
2024-04-17 17:08:52 -0300

    feat(task): starting development of tasks CRUD and other functions

--- tk_organizer/src/task.c
+++ tk_organizer/src/task.c
@@ -4,3 +4,89 @@ THIS IS A SOURCE FILE FOR A CODING PROJECT, EVERY LINE OF CODE PRODUCED
 SHALL FOLLOW GNU CODING STANDARDS DEFINED IN
 https://www.gnu.org/prep/standards/standards.html
 */
+
+#include <stdlib.h>
+#include <string.h>
+#include "task.h"
+
+
+// Task Status related functions
+int solve_task(task_t * task){
+	if ((task->status == DROPPED) || (task->status == SOLVED)){
+		return EXIT_FAILURE;
+	}
+	task->status = SOLVED;
+	return EXIT_SUCCESS;
+}
+
+int drop_task(task_t * task){
+	if ((task->status == DROPPED) || (task->status == SOLVED)){
+		return EXIT_FAILURE;
+	}
+	task->status = DROPPED;
+	return EXIT_SUCCESS;
+}
+
+int activate_task(task_t * task){	
+	if (task->status == ACTIVE){
+		return EXIT_FAILURE;
+	}
+	task->status = ACTIVE;
+	return EXIT_SUCCESS;
+}
+
+task_t * create_task(){	
+	task_t * task = (task_t *) malloc(sizeof(task_t));
+	if (task == NULL){
+		return (task_t *) NULL;
+	}
+	while (set_identifier(task, new_identifier()) != EXIT_SUCCESS);
+	while (set_metadata(task, get_metadata()) != EXIT_SUCCESS);
+	return task;
+}
+
+int change_name(task_t * task, char * new_name){
+	if (strlen(*new_name) > NAME_MAX_SIZE){
+		return EXIT_FAILURE;
+	}
+	if (strncpy(task->name, *new_name, NAME_MAX_SIZE) == NULL){
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+} 	
+
+int change_description(task_t * task, char * new_desc){
+	if (strlen(*new_desc) > NAME_MAX_SIZE){
+		return EXIT_FAILURE;
+	}
+	if (strncpy(task->description, *new_desc, DESCRIPTION_MAX_SIZE) == NULL){
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
+int set_identifier(task_t * task, id_t * identifier){
+	if (identifier == NULL){
+		return EXIT_FAILURE; 
+	}
+	if (!is_unique(identifier)){
+		return EXIT_FAILURE;	
+	}
+	task->identifier = *identifier;
+	return EXIT_SUCCESS;
+}
+
+int set_metadata(task_t * task, metadata_t * metadata){
+	if (metadata == NULL){
+		return EXIT_FAILURE;
+	}
+	if (!is_valid(metadata)){
+		return EXIT_FAILURE;
+	}
+	task->metadata = *metadata;
+	return EXIT_SUCCESS;
+}
+
+
+
+
--- tk_organizer/src/task.h
+++ tk_organizer/src/task.h
@@ -8,15 +8,36 @@ https://www.gnu.org/prep/standards/standards.html
 #ifndef __TASK.H__
 #define __TASK.H__
 
-#include <types.h>
+#include <stdint.h>
 
+#define ACTIVE 0
+#define SOLVED 1
+#define DROPPED -1
+
+#define NAME_MAX_SIZE 128
+#define DESCRIPTION_MAX_SIZE 512
+
+// Function signatures below
+
+// Status
+int solve_task(task_t *);
+int activate_task(task_t *);
+int drop_task(task_t *);
+
+// Create
+task_t * create_task();
+
+// Update
+int change_name(task_t *, char *);
+int change_description(task_t *, char *);
+int set_identifier(task_t *, id_t *);
 
 typedef struct
 task {
 
-	char * name;
-	char * description;
-	id_t id;
+	char name[NAME_MAX_SIZE];
+	char description[DESCRIPTION_MAX_SIZE];
+	id_t identifier;
 	metadata_t metadata;
 	tags_t tags;
 	uint8_t status;
