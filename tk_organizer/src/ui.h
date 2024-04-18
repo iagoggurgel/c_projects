3545a71ab70f0182e3c8d81bd29b98ca33e1c889
Iago Gouveia Gurgel DTop <iagoggurgel25@gmail.com>
2024-04-18 18:35:31 -0300

    feat(task): adding new function

---
+++ tk_organizer/src/ui.h
@@ -0,0 +1,9 @@
+/*
+AUTHOR: IAGO GOUVEIA GURGEL AND ZAÚ JÚLIO <zaujulio.dev@gmail.com>
+THIS IS A SOURCE FILE FOR A CODING PROJECT, EVERY LINE OF CODE PRODUCED
+SHALL FOLLOW GNU CODING STANDARDS DEFINED IN
+https://www.gnu.org/prep/standards/standards.html
+*/
+
+#include <ncurses.h>
+
